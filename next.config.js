d6ae56abc28a3a9fcbdf2d9e6bff86e7397d2502
Masaya <masaya.sasaki.99@gmail.com>
2022-07-06 08:19:22 +0900

    image optimization during static export

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const withOptimizedImages = require('next-optimized-images');
 
-module.exports = nextConfig
+module.exports = withOptimizedImages({
+  handleImages: ['jpeg', 'png', 'svg'],
+});
+
