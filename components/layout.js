3ce35d4df96fc4a8cb81b97ba9ae629ba290a283
Masaya <masaya.sasaki.99@gmail.com>
2022-07-06 08:38:38 +0900

    update

--- components/layout.js
+++ components/layout.js
@@ -21,7 +21,7 @@ export default function Layout({children}){
         <Link href='/'>
         <div className={styles.logos}>
         <image
-        src={require('../public/3dlogo.svg')}
+        src={require('../public/3dlogo.svg?original')}
         alt='logo'
         width='50px'
         height='50px'
