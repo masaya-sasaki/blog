5f12d39ab81b9d31521c0126d1157747840ebf8e
Masaya <masaya.sasaki.99@gmail.com>
2022-09-11 16:53:11 +0900

    add new project to favorites

--- components/projects.js
+++ components/projects.js
@@ -9,7 +9,7 @@ export default function Projects({featuredProjectsData}){
                 <h2 className={styles.title}>FEATURED PROJECTS</h2>
             <p className={styles.introduction}>
                 <div className={styles.landingbox}>
-                <ul style={{display: 'grid', gridTemplateColumns: '1fr', gap: '1rem', paddingInlineStart: '0px' }} >
+                <ul style={{display: 'grid', gridTemplateColumns: '1fr', gap: '3rem', paddingInlineStart: '0px' }} >
                 {featuredProjectsData.map(({ id, title, image, skills, link, githublink }) => (
                 <ProjectBox 
                 title={title}
---
+++ projects/lottery-contract.md
@@ -0,0 +1,10 @@
+---
+title: 'Lottery DApp'
+image: ''
+skills: 'DApp, Ethereum, Hardhat, React, Javascript'
+link: 'https://lottery-contract-test.netlify.app/'
+githublink: 'https://github.com/masaya-sasaki/lottery_practice_client'
+featured: 'yes'
+---
+Lottery DApp (Decentralized Application) - a lottery dapp on Goerli testnet where users can enter a lottery. 
+
